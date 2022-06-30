2f1a85edb6fea561aa7c085db353412240eba345
Vitaly Perekutnev <v.perekutnev@b2b-center.ru>
2022-06-30 22:11:44 +0300

    master add graphql

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir : __dirname, 
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    "@typescript-eslint/no-namespace": "off",
+    "comma-dangle": ["error", "never"],
+    "lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+    'max-len': [1, { code: 120 }],
+    "semi": ["error", "always", { "omitLastInOneLineBlock": true}] ,
+    "quotes": [2, "single"],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
