071d19b2dd56a1caabb481c4b462427fb447f1c4
a.drozdov <a.drozdov@b2b-center.ru>
2023-02-25 15:57:34 +0300

    activity

--- src/modules/activity/activity.controller.ts
+++ src/modules/activity/activity.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiBearerAuth, ApiDefaultResponse, ApiOperation } from '@nestjs/swagger';
+import { ActivityEntity } from '~/modules/activity/entities/activity.entity';
+import { ActivityService } from '~/modules/activity/activity.service';
+import { WsAction } from '@drozd/nestjs-ws-api';
 
 @Controller('activity')
-export class ActivityController {}
+export class ActivityController {
+
+  constructor(private readonly activityService: ActivityService) {}
+
+  @Get()
+  @ApiBearerAuth()
+  @ApiOperation({ operationId: 'readAll' })
+  @ApiDefaultResponse({ description: 'get all activities', type: [ActivityEntity] })
+  @HttpCode(200)
+  @WsAction('activity')
+  async readAll(): Promise<ActivityEntity[]> {
+    return await this.activityService.findAll();
+  }
+}
